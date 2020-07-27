0f11d868f4dafeee44fed7731184d829bcea7762
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-07-27 08:20:40 +0100

    Routing added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import "../styles/App.css";
+import { Switch, Route } from "react-router-dom";
+
 import NavBar from "./NavBar";
+import Skills from "./Skills";
+import AddSkills from "./AddSkills";
+
+import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Switch>
+        <Route exact path="/" component={Skills}>
+          <Skills />
+        </Route>
+        <Route exact path="/add-skills" component={AddSkills}>
+          <AddSkills />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/NavBar.css";
 
 const NavBar = () => {
   return (
     <div className="navbar">
       <ul className="navbar-links">
-        <li className="navbar-links-item">View Skills</li>
-        <li className="navbar-links-item">Add a Skill</li>
+        <li className="navbar-links-item">
+          <Link to="/">View Skills</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/add-skills">Add a Skill</Link>
+        </li>
       </ul>
     </div>
   );
